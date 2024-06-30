85fcdce39d77e159876117dbc91a7bf66e4b7036
steguiosaur <91378710+steguiosaur@users.noreply.github.com>
2024-06-30 19:49:10 +0800

    feat(vault): add modal frontend for vault creation

---
+++ js/vault_modal.js
@@ -0,0 +1,25 @@
+// modal variables
+var modal = document.getElementById("create-vault-modal");
+var btn = document.getElementById("add-vault-btn");
+var createBtn = document.getElementById("create-vault");
+var cancelBtn = document.getElementById("cancel-vault");
+
+// open modal when Add Vault is pressed
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// close on all these remaining behaviors
+cancelBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+createBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
--- vault.php
+++ vault.php
@@ -5,7 +5,8 @@ require 'layouts/navbar.php';
 
 <div class="main">
     <div class="container">
-        <div class="banner">
+        <div class="contain-row">
+            <div class="banner">
 
 <?php
 // check if user is logged in
@@ -25,32 +26,50 @@ $join_year = $_SESSION['created_at'];
 $profile_picture = 'img/account.png';
 ?>
 
-            <img class="profile-img" src="<?php echo $profile_picture; ?>" alt="Profile Picture" />
-            <div class="profile-info">
-                <h4 class="profile-name"><?php echo $username; ?></h4>
-                <p>ID: <?php echo $user_id; ?></p>
-                <p>Email: <?php echo $email; ?></p>
-                <p>Joined <?php echo $join_year; ?></p>
-                <form action="sessions/session_out.php" method="POST">
-                    <button type="submit" name="logout">Sign Out</button>
-                </form>
+                <img class="profile-img" src="<?php echo $profile_picture; ?>" alt="Profile Picture" />
+                <div class="profile-info">
+                    <h4 class="profile-name"><?php echo $username; ?></h4>
+                    <p>ID: <?php echo $user_id; ?></p>
+                    <p>Email: <?php echo $email; ?></p>
+                    <p>Joined <?php echo $join_year; ?></p>
+                    <form action="sessions/session_out.php" method="POST">
+                        <button type="submit" name="logout">Sign Out</button>
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="vaults">
-            <h4>Your Vault</h4>
-            <section class="vault-section">
-                <button class="btn-outline" type="button">Add Vault</button>
-                <button class="btn-fill" type="button">Machine Learning</button>
-                <button class="btn-fill" type="button">Cybersecurity</button>
-                <button class="btn-fill" type="button">Software Engineering</button>
-            </section>
-            <h4>Other Vault</h4>
-            <section class="vault-section">
-                <button class="btn-fill" type="button">WebDev</button>
-                <button class="btn-fill" type="button">Thesis</button>
-            </section>
+            <div class="vaults">
+                <h4>Your Vault</h4>
+                <section class="vault-section">
+                    <button class="btn-outline" id="add-vault-btn" type="button">Add Vault</button>
+                    <button class="btn-fill" type="button">Machine Learning</button>
+                    <button class="btn-fill" type="button">Cybersecurity</button>
+                    <button class="btn-fill" type="button">Software Engineering</button>
+                </section>
+                <h4>Other Vault</h4>
+                <section class="vault-section">
+                    <button class="btn-fill" type="button">WebDev</button>
+                    <button class="btn-fill" type="button">Thesis</button>
+                </section>
+            </div>
         </div>
     </div>
 </div>
 
+<div id="create-vault-modal" class="modal">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h3>Create Vault</h3>
+    </div>
+    <div class="modal-body">
+      <input type="text" id="vault-name" placeholder="Enter vault name">
+    </div>
+    <div class="modal-buttons">
+      <button class="btn-create" id="create-vault">Create</button>
+      <button class="btn-cancel" id="cancel-vault">Cancel</button>
+    </div>
+  </div>
+</div>
+
 <?php require 'layouts/footer.php'; ?>
+
+<script src="js/vault_modal.js"></script>
